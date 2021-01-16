23b7919fe6c7342807f2fcd35e11a0e3cffd6fea
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-16 18:33:13 +0530

    Change makefile guard to surya
    BOARD_VENDOR is a deprecated flag and no one uses it anymore,
    and this port of micam is only for surya/karna anyway
    
     * simplify the lib path while we're at it

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter xiaomi,$(BOARD_VENDOR)),)
+ifeq ($(TARGET_DEVICE),surya)
 
 CAM_LIBS := libclone_c++_shared.so libcom.xiaomi.camera.algojni.so libcom.xiaomi.camera.mianodejni.so libcom.xiaomi.camera.requestutil.so libDocumentProcess_qcom.so libfenshen_snpe.so libffmpeg.so libjni_jpegutil_xiaomi.so libmulti-wakeup-engine.so librecord_video.so libvvc++_shared.so libDocumentProcess.so
-CAM_SYMLINKS := $(addprefix $(PRODUCT_OUT)/system/priv-app/MiuiCamera/lib/arm64/,$(notdir $(CAM_LIBS)))
+CAM_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/MiuiCamera/lib/arm64/,$(notdir $(CAM_LIBS)))
 $(CAM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Cam lib link: $@"
 	@mkdir -p $(dir $@)
