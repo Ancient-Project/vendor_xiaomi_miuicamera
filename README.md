af6515e1135fa657b89f5b37f4f5005fff9c7b72
rezaadi0105 <rezaadipangestu5@gmail.com>
2021-06-11 18:21:59 +0700

    violet: fixes 48mp
    extract from modules 48mp fix
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+Add broken rules in BoardConfig tree for replace camera stuff from source:
+
+```bash
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
+```
--- config.mk
+++ config.mk
@@ -2,7 +2,10 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/miuicamera
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64)
 
 PRODUCT_PACKAGES += \
     ANXCamera
